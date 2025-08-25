9c4e7e295e1fe470c192d47a4e098991f4d2d61c
ametriyadhi <94771711+ametriyadhi@users.noreply.github.com>
2025-08-25 12:02:31 +0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -29,7 +29,7 @@ export enum AkunTipe {
 }
 
 export interface Akun {
-  id: string; // Ditambahkan untuk referensi
+  id: string;
   kode: string; nama: string; tipe: AkunTipe; parent_kode?: string; saldo: number;
 }
 
@@ -41,7 +41,6 @@ export interface LaporanArsip {
   id: string; namaLaporan: string; tanggalDibuat: string; dataLaporan: ReportRow[];
 }
 
-// --- TIPE DATA BARU UNTUK JURNAL ---
 export interface JurnalEntryLine {
     akun_id: string;
     akun_kode: string;
@@ -52,24 +51,21 @@ export interface JurnalEntryLine {
 
 export interface JurnalEntry {
     id: string;
-    tanggal: string; // ISO string
+    tanggal: string;
     deskripsi: string;
     lines: JurnalEntryLine[];
 }
 
-// --- TIPE DATA PENGATURAN DIPERBARUI DI SINI ---
 export interface AppSettings {
   id?: string;
   simpanan_pokok: number;
   simpanan_wajib: number;
-  // Kebijakan Margin (dalam persen, misal 10 untuk 10%)
   margin_tenor_6: number; 
   margin_tenor_12: number;
   margin_tenor_18: number;
   margin_tenor_24: number;
-  // Kebijakan Plafon & Cicilan
-  plafon_pembiayaan_gaji: number; // Pengali gaji, misal 5 untuk 5x gaji
-  maksimal_cicilan_gaji: number; // Pembagi gaji, misal 3 untuk 1/3 gaji
+  plafon_pembiayaan_gaji: number;
+  maksimal_cicilan_gaji: number;
 }
 
 // --- TIPE DATA BARU UNTUK PERAN & PENGGUNA ---
@@ -79,5 +75,4 @@ export interface UserProfile {
     uid: string;
     email: string;
     role: UserRole;
-
-
+}
