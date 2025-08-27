453a810c35039fcb1637f872e187a96fe2f9dc93
ametriyadhi <94771711+ametriyadhi@users.noreply.github.com>
2025-08-27 22:05:52 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -20,6 +20,15 @@ export const UsersIcon: React.FC<IconProps> = ({ className = 'w-6 h-6' }) => (
   </svg>
 );
 
+export const ArrowDownUpIcon: React.FC<IconProps> = ({ className = 'w-6 h-6' }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <path d="m3 16 4 4 4-4"/>
+        <path d="M7 20V4"/>
+        <path d="m21 8-4-4-4 4"/>
+        <path d="M17 4v16"/>
+    </svg>
+);
+
 export const PencilIcon: React.FC<IconProps> = ({ className = 'w-6 h-6' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
         <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
